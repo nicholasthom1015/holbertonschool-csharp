8ab7b70714ff2de801d0d13bd575f20d0143b837
root <nicholasthomas10.15@gmail.com>
2023-05-15 15:58:32 +0000

    fixesss

--- csharp-arrays_lists_dictionaries/0-print_array/0-print_array.cs
+++ csharp-arrays_lists_dictionaries/0-print_array/0-print_array.cs
@@ -11,7 +11,7 @@ public class Array
         }
         int[] res = new int[size];
         for(int i = 0; i < size; i++)
-            Console.WriteLine("{0}{1}",(res[i] = i), i == (size-1)?"":" ");
+            Console.WriteLine("{0}{1}",(res[i] = i), i == (size-1)?"":" \n");
         Console.WriteLine();
         return res;
     }
