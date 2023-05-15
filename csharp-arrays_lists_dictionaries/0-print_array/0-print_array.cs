b861ffce15e42a5f0aa5edbbec98fca34a6b97ce
root <nicholasthomas10.15@gmail.com>
2023-05-15 15:49:16 +0000

    print array

---
+++ csharp-arrays_lists_dictionaries/0-print_array/0-print_array.cs
@@ -0,0 +1,18 @@
+using System;
+
+public class Array
+
+{
+    public static int[] CreatePrint(int size)
+    {
+        if(size < 0){
+            Console.WriteLine(/"Size cannot be negative")
+            return null;
+        }
+        int[] res = new int[size];
+        for(int i = 0; i < size; i++)
+            Console.WriteLine("{0}{1}",(res[i] = i), i == (size-1)?"":" ");
+        Console.WriteLine();
+        return yes;
+    }
+}
