26ab686584768cb14de84cee0ed083e4c397d1af
root <nicholasthomas10.15@gmail.com>
2023-05-15 16:56:23 +0000

    square matrix

---
+++ csharp-arrays_lists_dictionaries/15-square_matrix/15-square_matrix.cs
@@ -0,0 +1,14 @@
+using System;
+
+public class Matrix
+{
+    public static int [,] Square(int [,] myMatrix)
+    {
+        int[,] squareMatrix = new int[myMatrix.GetLength(0), myMatrix.GetLength(1)];
+
+        for(int i = 0; i < myMatrix.GetLength(0); i++)
+            for(int j = 0; j < myMatrix.GetLength(1); j++)
+                squareMatrix[i, j] = myMatrix[i, j] * myMatrix[i, j];
+        return squareMatrix;
+    }
+}
