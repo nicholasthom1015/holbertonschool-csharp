e45cdbe55ef5fbb357142e0535584df8219dfb04
root <nicholasthomas10.15@gmail.com>
2023-05-24 01:28:38 +0000

    is

---
+++ csharp-inheritance/0-is/0-is.cs
@@ -0,0 +1,11 @@
+using System;
+
+    /// <summary> Object class </summary>
+    public static class Obj
+    {
+        /// <summary> IsOfTypeInt method </summary>
+        public static bool IsOfTypeInt(object obj)
+        {
+            return obj is int;
+        }
+    }
