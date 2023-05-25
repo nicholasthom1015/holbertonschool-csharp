a484f6437852262ca229f202dc3bb5e8f6f17f55
root <nicholasthomas10.15@gmail.com>
2023-05-25 15:33:40 +0000

    task 2

---
+++ csharp-inheritance/2-subclass/2-subclass.cs
@@ -0,0 +1,9 @@
+using System;
+
+public class Obj
+{
+    public static bool IsOnlyASubclass(Type derivedType, Type baseType)
+    {
+        return baseType.IsAssignableFrom(derivedType) && derivedType != baseType;
+    }
+}
