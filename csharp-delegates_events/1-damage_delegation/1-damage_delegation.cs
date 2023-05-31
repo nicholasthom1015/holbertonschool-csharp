ce6602c243bc45600ad979e5588ab6488c4635d0
root <nicholasthomas10.15@gmail.com>
2023-05-31 02:39:10 +0000

    damage

--- csharp-delegates_events/1-damage_delegation/1-damage_delegation.cs
+++ csharp-delegates_events/1-damage_delegation/1-damage_delegation.cs
@@ -1,51 +1,55 @@
-using System;
+﻿using System;
 
 
-public delegate void CalculateHealth(float amount);
-
+/// <summary> Player class </summary>
 public class Player
 {
-    private string name;
-    private float maxHp;
-    private float hp;
+    /// <summary> Player name </summary>
+    protected string name;
+    /// <summary> Player maxHp </summary>
+    protected float maxHp;
+    /// <summary> Player hp </summary>
+    protected float hp;
 
-    public Player(string name, float maxHp)
+    /// <summary> Player delegate </summary>
+    public delegate void CalculateHealth(float amount);
+
+    /// <summary> Player Constructor </summary>
+    public Player(string name="Player", float maxHp=100f)
     {
         this.name = name;
-        if (maxHp > 0)
-        {
-            this.maxHp = maxHp;
-        }
-        else
-        {
-            this.maxHp = 100f;
+        if( maxHp <= 0f){
             Console.WriteLine("maxHp must be greater than 0. maxHp set to 100f by default.");
+            maxHp = 100f;
         }
-        hp = this.maxHp;
+        this.maxHp = maxHp;
+        this.hp = this.maxHp;
     }
 
+    /// <summary> PrintHealth Method </summary>
     public void PrintHealth()
     {
-        Console.WriteLine($"{name} has {hp} / {maxHp} health");
+        Console.WriteLine("{0} has {1} / {2} health", name, hp, maxHp);
     }
 
+    /// <summary> TakeDamage Method </summary>
     public void TakeDamage(float damage)
     {
-        if (damage < 0)
-        {
-            damage = 0;
-        }
-        hp -= damage;
-        Console.WriteLine($"{name} takes {damage} damage!");
+        if( damage < 0f)
+            damage = 0f;
+            Console.WriteLine("{0} takes {1} damage!", name, damage);
+            hp -= damage;
+            hp = math.clamp(hp, 0f, maxHp);
     }
 
+    /// <summary> HealDamage Method </summary>
     public void HealDamage(float heal)
     {
-        if (heal < 0)
-        {
-            heal = 0;
-        }
+        if( heal < 0f)
+            heal = 0f;
+        Console.WriteLine("{0} heals {1} HP!", name, heal);
         hp += heal;
-        Console.WriteLine($"{name} heals {heal} HP!");
+        hp = math.clamp(hp, 0f, maxHp);
     }
+
 }
