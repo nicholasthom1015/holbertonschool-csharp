e2910313c5243c20cdce2a21a4ca1ac6ef4e9e19
root <nicholasthomas10.15@gmail.com>
2023-05-31 01:51:42 +0000

    guess not

--- csharp-delegates_events/1-damage_delegation/1-damage_delegation.cs
+++ csharp-delegates_events/1-damage_delegation/1-damage_delegation.cs
@@ -1,34 +1,5 @@
 using System;
 
-/// <summary> Player class </summary>
-public class Player
-{
-    /// <summary> Player name </summary>
-    protected string name;
-    /// <summary> Player maxHp </summary>
-    protected float maxHp;
-    /// <summary> Player hp </summary>
-    protected float hp;
-
-    /// <summary> Player Constructor </summary>
-    public Player(string name="Player", float maxHp=100f)
-    {
-        this.name = name;
-        if( maxHp <= 0f){
-            Console.WriteLine("maxHp must be greater than 0. maxHp set to 100f by default.");
-            maxHp = 100f;
-        }
-        this.maxHp = maxHp;
-        this.hp = this.maxHp;
-    }
-
-    /// <summary> PrintHealth Method </summary>
-    public void PrintHealth()
-    {
-        Console.WriteLine("{0} has {1} / {2} health", name, hp, maxHp);
-    }
-
-}
 
 public delegate void CalculateHealth(float amount);
 
