13b46217b1033f85efcef4d20a95e772eb65ef98
root <nicholasthomas10.15@gmail.com>
2023-05-31 03:34:21 +0000

    yay

---
+++ csharp-delegates_events/2-validation/2-validation.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+
+/// <summary> Player class </summary>
+public class Player
+{
+    /// <summary> Player name </summary>
+    protected string name;
+    /// <summary> Player maxHp </summary>
+    protected float maxHp;
+    /// <summary> Player hp </summary>
+    protected float hp;
+
+    /// <summary> Player delegate </summary>
+    public delegate void CalculateHealth(float amount);
+
+    /// <summary> Player Constructor </summary>
+    public Player(string name="Player", float maxHp=100f)
+    {
+        this.name = name;
+        if( maxHp <= 0f){
+            Console.WriteLine("maxHp must be greater than 0. maxHp set to 100f by default.");
+            maxHp = 100f;
+        }
+        this.maxHp = maxHp;
+        this.hp = this.maxHp;
+    }
+
+    /// <summary> PrintHealth Method </summary>
+    public void PrintHealth()
+    {
+        Console.WriteLine("{0} has {1} / {2} health", name, hp, maxHp);
+    }
+
+    /// <summary> TakeDamage Method </summary>
+    public void TakeDamage(float damage)
+    {
+        if( damage < 0f)
+            damage = 0f;
+        Console.WriteLine("{0} takes {1} damage!", name, damage);
+        ValidateHP(hp - damage);
+    }
+
+    /// <summary> HealDamage Method </summary>
+    public void HealDamage(float heal)
+    {
+        if( heal < 0f)
+            heal = 0f;
+        Console.WriteLine("{0} heals {1} HP!", name, heal);
+        ValidateHP(hp + heal);
+
+    }
+
+    /// <summary> Validates HP </summary>
+    public void ValidateHP(float newHp)
+    {
+        hp = Math.Clamp(newHp, 0, maxHp);
+    }
+}
