db975081d06e8534647ad1ddaba8944b0b4bed3d
root <nicholasthomas10.15@gmail.com>
2023-05-31 02:37:07 +0000

    /

--- csharp-generics/0-queue/queue.cs
+++ csharp-generics/0-queue/queue.cs
@@ -1,10 +1,12 @@
 using System;
 
-/// <summary> Queue class </summary>
+// <summary> Queue class </summary>
+
 public class Queue<T>
 {
 
-    /// <summary> Returns the Queue's type </summary>
+// <summary> Returns the Queue's type </summary>
+
     public Type CheckType()
     {
         return typeof(T);
